5b95070c4619e0298c43a8782c1caf05230bb265
ilyaeighteen <95511904+ilyaeighteen@users.noreply.github.com>
2023-11-09 14:11:47 +0300

    1st commit

---
+++ cmd/test-application/config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+	logs "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+var Config appConfig
+
+// определяем новый тип для хранения конфигурации, при добавлении новых параметров необходим переопределять!!!
+type appConfig struct {
+	DB_HOST       string `mapstructure:"db_host"`
+	DB_USER       string `mapstructure:"db_user"`
+	DB_PASSWD     string `mapstructure:"db_passwd"`
+	DB            string `mapstructure:"db"`
+	DB_PORT       string `mapstructure:"db_port"`
+}
+
+// перечитываем файл конфигурации
+func ReloadConfig() error {
+	absolute_path := "/var/opt/config"
+	if config_err := LoadConfig(absolute_path); config_err != nil {
+		logs.WithFields(logs.Fields{
+			"ReloadConfig": "Open file",
+			"PATH":         absolute_path,
+		}).Fatal(config_err.Error())
+		return config_err
+	}
+	logs.WithFields(logs.Fields{
+		"ReloadConfig": "Open file",
+		"PATH":         absolute_path,
+	}).Info("Config file Reload!")
+	return nil
+}
+
+// читаем конфигурацию из файла
+func LoadConfig(configPaths ...string) error {
+	v := viper.New()
+	v.SetConfigName("conf") // <- имя конфигурационного файла
+	v.SetConfigType("json")
+	v.SetEnvPrefix("blueprint")
+	v.AutomaticEnv()
+	for _, path := range configPaths {
+		v.AddConfigPath(path) // <- // путь для поиска конфигурационного файла в
+	}
+	if err := v.ReadInConfig(); err != nil {
+		return fmt.Errorf("failed to read the configuration file: %s", err)
+	}
+	//парсим json в переменную
+	return v.Unmarshal(&Config)
+}
---
+++ cmd/test-application/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"time"
+	"test-application/v1.0.0/v2/cmd/test-application/config"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+type User struct {
+	ID        int
+	Name      string
+	CreatedAt time.Time
+}
+
+var db *sql.DB
+
+func main() {
+	// Инициализация базы данных PostgreSQL
+	var err error
+	connStr := "postgres://" + config.Config.DB_USER + ":" + config.Config.DB_PASSWD + "@" + config.Config.DB_HOST + ":" + config.Config.DB_PORT + "/" + config.Config.DB + "?sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Создание таблицы пользователей, если ее нет
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			name TEXT,
+			created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+		)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Создание экземпляра Gin-приложения
+	r := gin.Default()
+
+	// Маршрут для отображения главной страницы
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "templates/index.html", nil)
+	})
+
+	// Маршрут для обработки данных пользователя
+	r.POST("/greet", func(c *gin.Context) {
+		name := c.PostForm("name")
+
+		// Записываем пользователя в базу данных
+		_, err := db.Exec("INSERT INTO users (name) VALUES ($1)", name)
+		if err != nil {
+			log.Println(err)
+			c.String(http.StatusInternalServerError, "Ошибка при записи в базу данных")
+			return
+		}
+
+		// Получаем последних 5 пользователей из базы данных
+		users, err := getUsers()
+		if err != nil {
+			log.Println(err)
+			c.String(http.StatusInternalServerError, "Ошибка при чтении из базы данных")
+			return
+		}
+
+		// Отправляем список пользователей на страницу
+		c.HTML(http.StatusOK, "templates/greet.html", gin.H{
+			"users": users,
+		})
+	})
+
+	// Запуск веб-сервера на порту 8080
+	r.Run(":8080")
+}
+
+func getUsers() ([]User, error) {
+	// Запрос к базе данных для получения последних 5 пользователей
+	rows, err := db.Query("SELECT id, name, created_at FROM users ORDER BY created_at DESC LIMIT 5")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Итерация по результатам запроса и создание списка пользователей
+	var users []User
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Name, &user.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
